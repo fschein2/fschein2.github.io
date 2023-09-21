6fe91121a416e2227afc439667547086be7aad17
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-09-21 18:31:27 -0400

    completed assignment 7 and added it to main page

--- assignments/assignment7/script.js
+++ assignments/assignment7/script.js
@@ -23,13 +23,65 @@ const displayOrder = () => {
     const name1 = document.getElementById("txt-name1").value;
     const name2 = document.getElementById("txt-name2").value;
     const name3 = document.getElementById("txt-name3").value;
-    /* need to make it compare and list names by age order o-y
-       if invalid data is in age cells show error message */
-    if(isNaN(age1)) {
-        /* create a hidden class for the error message and make it appear
-        next to the appropriate age */
-    } else if(age1 < 0) {
+    const comparison = document.getElementById("comparison");
 
+    if(isNaN(age1) || isNaN(age2) || isNaN(age3)) {
+        comparison.innerHTML = "At least one value for age is not a number";
+    } else if(age1 < 0 || age2 < 0 || age3 < 0) {
+        comparison.innerHTML = "At least one value for age is less than 0";
+    } 
+
+    if(age1 != age2 && age1 != age3 && age2 != age3) {
+        if(age1 > age2 && age2 > age3) {
+            comparison.innerHTML = `${name1}, ${name2}, ${name3}.`;
+        } else if(age1 > age3 && age3 > age2) {
+            comparison.innerHTML = `${name1}, ${name3}, ${name2}.`;
+        } else if(age2 > age1 && age1 > age3) {
+            comparison.innerHTML = `${name2}, ${name1}, ${name3}.`;
+        } else if(age2 > age3 && age3 > age1) {
+            comparison.innerHTML = `${name2}, ${name3}, ${name1}.`;
+        } else if(age3 > age1 && age1 > age2) {
+            comparison.innerHTML = `${name3}, ${name1}, ${name2}.`;
+        } else if(age3 > age2 && age2 > age1) {
+            comparison.innerHTML = `${name3}, ${name2}, ${name1}.`;
+        }
+    } else if(age1 == age2 && age2 == age3) {
+        comparison.innerHTML = `${name1}, ${name2}, ${name3}.`;
+    } else if(age1 == age2 && age2 != age3) {
+        if(age1 > age3) {
+            comparison.innerHTML = `${name1}, ${name2}, ${name3}.`;
+        } else if(age3 > age1) {
+            comparison.innerHTML = `${name3}, ${name1}, ${name2}.`;
+        }
+    } else if(age1 == age3 && age3 != age2) {
+        if(age1 > age2) {
+            comparison.innerHTML = `${name1}, ${name3}, ${name2}.`;
+        } else if(age2 > age1) {
+            comparison.innerHTML = `${name2}, ${name1}, ${name3}.`;
+        }
+    } else if(age2 == age3 && age3 != age1) {
+        if(age2 > age1) {
+            comparison.innerHTML = `${name2}, ${name3}, ${name1}.`;
+        } else if(age1 > age2) {
+            comparison.innerHTML = `${name1}, ${name2}, ${name3}.`;
+        }
+    }
+};
+
+const fillThermometer = () => {
+    const fundsRaised = parseInt(document.getElementById("txt-funds").value);
+    const root = document.querySelector(":root");
+
+    if(fundsRaised < 2500) {
+        root.style.setProperty("--amount", "0%");
+    } else if(fundsRaised < 5000 && fundsRaised >= 2500) {
+        root.style.setProperty("--amount", "25%");
+    } else if(fundsRaised < 7500 && fundsRaised >= 5000) {
+        root.style.setProperty("--amount", "50%");
+    } else if(fundsRaised < 10000 && fundsRaised >= 7500) {
+        root.style.setProperty("--amount", "75%");
+    } else if(fundsRaised >= 10000) {
+        root.style.setProperty("--amount", "100%");
     }
 };
 
@@ -39,4 +91,5 @@ window.onload = () => {
     document.getElementById("exercise1").onclick = showExercise1;
     document.getElementById("exercise2").onclick = showExercise2;
     document.getElementById("compare-button").onclick = displayOrder;
+    document.getElementById("display-button").onclick = fillThermometer;
 };
