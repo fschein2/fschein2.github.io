65fd0a632f1687f8a6380627de491cc4fe017689
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-09-21 12:44:00 -0400

    made minor changes and set up for rest of the assignment

--- assignments/assignment7/script.js
+++ assignments/assignment7/script.js
@@ -5,22 +5,32 @@ const toggleNav = () => {
 const showExercise1 = () => {
     document.getElementById("fundraiser").classList.add("hide2");
     document.getElementById("oldest").classList.remove("hide1");
+    document.getElementById("exercise1").classList.add("selected1");
+    document.getElementById("exercise2").classList.remove("selected2"); 
 };
 
 const showExercise2 = () => {
     document.getElementById("oldest").classList.add("hide1");
     document.getElementById("fundraiser").classList.remove("hide2");
+    document.getElementById("exercise1").classList.remove("selected1");
+    document.getElementById("exercise2").classList.add("selected2");
 };
 
 const displayOrder = () => {
-    const age1 = document.getElementById("txt-age1").value;
-    const age2 = document.getElementById("txt-age2").value;
-    const age3 = document.getElementById("txt-age3").value;
+    const age1 = parseInt(document.getElementById("txt-age1").value);
+    const age2 = parseInt(document.getElementById("txt-age2").value);
+    const age3 = parseInt(document.getElementById("txt-age3").value);
     const name1 = document.getElementById("txt-name1").value;
     const name2 = document.getElementById("txt-name2").value;
     const name3 = document.getElementById("txt-name3").value;
     /* need to make it compare and list names by age order o-y
        if invalid data is in age cells show error message */
+    if(isNaN(age1)) {
+        /* create a hidden class for the error message and make it appear
+        next to the appropriate age */
+    } else if(age1 < 0) {
+
+    }
 };
 
 /* Need to make it load with exercise 1 on screen */
