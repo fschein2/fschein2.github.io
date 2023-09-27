17839c044df39266ccbffc1f267f67f2b7b64bc6
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-09-27 18:22:18 -0400

    completed thermometer part of assignment 8

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -1,1 +1,22 @@
-console.log("Hi Fred!")
+const setThermometer = () => {
+    const end = parseInt(document.getElementById("txt-funds").value);
+    const root = document.querySelector(":root");
+    let count = 0;
+    const total = 10000;
+    let percent = 0;
+
+    const updateThermometer = setInterval(() => {
+        count++;
+        percent = (count * 100)/total;
+        root.style.setProperty("--amount", `${percent}%`);
+
+        if (count >= end) {
+            clearInterval(updateThermometer);
+        }
+
+    }, 1);
+}
+
+window.onload = () => {
+    document.getElementById("display-button").onclick = setThermometer;
+}
