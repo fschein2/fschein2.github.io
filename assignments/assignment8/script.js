072a477b3b6135e6cdd260d7e5a6f89fab8fad30
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-09-28 13:03:48 -0400

    made image move now need to make it change

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -1,6 +1,26 @@
 const runMan = () => {
+    let box = document.querySelector('#running-man');
+    let width = box.clientWidth;
+    const end = width - 80;
+    const root = document.querySelector(":root");
+    let count = 10;
     
-}
+    const moveMan = setInterval(() => {
+        count++;;
+        root.style.setProperty("--position", `${count}px`);
+
+        if (count % 2 == 0) {
+
+        } else {
+
+        }     
+
+        if (count >= end) {
+            clearInterval(moveMan);
+        }
+
+    }, 1);
+};
 
 const setThermometer = () => {
     const end = parseInt(document.getElementById("txt-funds").value);
@@ -19,7 +39,7 @@ const setThermometer = () => {
         }
 
     }, 1);
-}
+};
 
 window.onload = () => {
     document.getElementById("display-button").onclick = setThermometer;
