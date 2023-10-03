20e57fb8fc823eeb9c7b17ea6c16027f22eb7afb
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-03 18:10:42 -0400

    added oct3 notes and completed html and css for assignment9. completed quotes part of js for asignment 9

---
+++ assignments/assignment9/script.js
@@ -0,0 +1,26 @@
+const displayQuotes = () => {
+    const quotes = [
+        "\"The computer was born to solve problems that did not exist before.\" — Bill Gates",
+        "\"Before software should be reusable, it should be usable.\"— Ralph Johnson",
+        "\"A program is never less than 90% complete, and never more than 95% complete.\"— Terry Baker",
+        "\"Design is where science and art break even.\" - Robin Matthews",
+        "\"Everybody should learn to program a computer, because it teaches you how to think.\" - Steve Jobs"
+    ]
+
+    const quoteArea = document.getElementById("quotes");
+    let i = 0;
+
+    quoteArea.innerHTML = quotes[i];
+
+    const rotateQuotes = setInterval( () => {
+        i++;
+        if (i >= quotes.length) {
+            i = 0;
+        }
+        quoteArea.innerHTML = quotes[i];
+    }, 2000);
+};
+
+window.onload = () => {
+    displayQuotes();
+};
