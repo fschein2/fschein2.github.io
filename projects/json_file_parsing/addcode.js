5d3a8b32dafb17964164f72eee4f075f44f2db04
Fred Schein <fschein@Freds-MacBook-Pro-9.local>
2023-10-26 23:03:09 -0400

    missed save

--- projects/json_file_parsing/addcode.js
+++ projects/json_file_parsing/addcode.js
@@ -13,7 +13,54 @@ const addCode = (e) => {
     console.log(image);
 
     const result = document.getElementById("code-result");
-    result.innerHTML = "Your code has been submitted and will be added upon review.";
+    result.innerHTML = `Your code has been submitted and will be added upon review.
+                        <br>Type of Code: ${type}<br>Explanation: ${explanation}
+                        <br>Link: ${link}<br>Image: ${image}`;
 };
 
-document.getElementById("add-code-form").onsubmit = addCode;
+const loadCode = async () => {
+    const url = "https://fschein2.github.io/json/javaCode.json";
+
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showCode = async () => {
+    const codeList = await loadCode();
+    const codeDiv = document.getElementById("code-style");
+
+    codeList.forEach((code) => {
+        codeDiv.append(getCodeItem(code));
+    });
+};
+
+const getCodeItem = (code) => {
+    const div = document.createElement("div");
+
+    const h1 = document.createElement("h1");
+    div.append(h1);
+    h1.innerHTML = code.codeType;
+
+    const img = document.createElement("img");
+    div.append(img);
+    img.src = code.image;
+
+    const h3 = document.createElement("h3");
+    div.append(h3);
+    h3.innerHTML = "Explanation:";
+
+    const p = document.createElement("p");
+    div.append(p);
+    p.innerHTML = code.explanation;
+
+    const a = document.createElement("a");
+    div.append(a);
+    a.innerHTML = `<strong>Link</strong>`;
+    a.href = code.link;
+
+    return div;
+};
